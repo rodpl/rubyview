ea3bcd2eceda1655c7d34ef25fc5f722e26da09c
dabrowski.daniel <dabrowski.daniel@b5b26504-4a54-0410-b002-3927155f929f>
2008-08-22 22:10:33 +0000

    Added performance tests

--- trunk/src/app/Castle.MonoRail.Views.RubyView/RubyViewEngine.cs
+++ trunk/src/app/Castle.MonoRail.Views.RubyView/RubyViewEngine.cs
@@ -2,135 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Security;
-using System.Text;
 using Castle.MonoRail.Framework;
-using Microsoft.Scripting.Hosting;
 using Ruby;
-[assembly: AllowPartiallyTrustedCallers()]
+
 namespace Castle.MonoRail.Views.RubyView
 {
 	public class RubyViewEngine : ViewEngineBase
 	{
 		private const string VIEW_FILE_EXTENSION = ".erb";
-
-		/// <summary>
-		/// Implementors should return a generator instance if
-		/// the view engine supports JS generation.
-		/// </summary>
-		/// <param name="generatorInfo">The generator info.</param>
-		/// <param name="context">The request context.</param>
-		/// <param name="controller">The controller.</param>
-		/// <param name="controllerContext">The controller context.</param>
-		/// <returns>A JS generator instance</returns>
-		public override object CreateJSGenerator(JSCodeGeneratorInfo generatorInfo, IEngineContext context, IController controller, IControllerContext controllerContext)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Processes the js generation view template - using the templateName
-		/// to obtain the correct template, and using the specified <see cref="T:System.IO.TextWriter"/>
-		/// to output the result.
-		/// </summary>
-		/// <param name="templateName">Name of the template.</param>
-		/// <param name="output">The output.</param>
-		/// <param name="generatorInfo">The generator info.</param>
-		/// <param name="context">The request context.</param>
-		/// <param name="controller">The controller.</param>
-		/// <param name="controllerContext">The controller context.</param>
-		public override void GenerateJS(string templateName, TextWriter output, JSCodeGeneratorInfo generatorInfo, IEngineContext context, IController controller, IControllerContext controllerContext)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Processes the view - using the templateName
-		/// to obtain the correct template
-		/// and writes the results to the System.IO.TextWriter.
-		/// </summary>
-		/// <param name="templateName"></param>
-		/// <param name="output"></param>
-		/// <param name="context"></param>
-		/// <param name="controller"></param>
-		/// <param name="controllerContext"></param>
-		/// <exception cref="MonoRailException"><c>MonoRailException</c>.</exception>
-		public override void Process(string templateName, TextWriter output, IEngineContext context, IController controller, IControllerContext controllerContext)
-		{
-			var viewFileName = string.Concat(templateName, ViewFileExtension);
-
-			var viewSource = ViewSourceLoader.GetViewSource(viewFileName);
-			if(viewSource == null)
-				throw new MonoRailException(404, "No such view", string.Format("Missing or invalid view: {0}", viewFileName));
-			var layoutNames = controllerContext.LayoutNames;
-
-			var scriptRuntime = IronRuby.CreateRuntime();
-			var scriptScope = scriptRuntime.CreateScope();
-			var scriptEngine = IronRuby.GetEngine(scriptRuntime);
-
-			using (var reader = new StreamReader(viewSource.OpenViewStream()))
-			{
-				var view = new RubyView(viewFileName, reader, output);
-				if (layoutNames != null)
-				{
-					var last = view;
-
-					for (int i = layoutNames.Length - 1; i >= 0; i--)
-					{
-						Debug.Write(i);
-						IViewSource layoutSource;
-						var layoutFileName = string.Concat("Layouts\\", layoutNames[i].Trim(), ViewFileExtension);
-						layoutSource = ViewSourceLoader.GetViewSource(layoutFileName);
-						if (layoutSource == null)
-							throw new MonoRailException(string.Format("Layout '{0}' cannot be found or loaded.", layoutNames[i].Trim()));
-						last.Parent = new RubyView(layoutFileName, new StreamReader(layoutSource.OpenViewStream()), output);
-						last = last.Parent;
-					}
-				}
-				view.Render();
-			}
-		}
-
-		/// <summary>
-		/// Processes the view - using the templateName
-		/// to obtain the correct template
-		/// and writes the results to the <see cref="T:System.IO.TextWriter"/>.
-		/// </summary>
-		/// <param name="templateName"></param>
-		/// <param name="layoutName"></param>
-		/// <param name="output"></param>
-		/// <param name="parameters"></param>
-		public override void Process(string templateName, string layoutName, TextWriter output, IDictionary<string, object> parameters)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Wraps the specified content in the layout using the
-		/// context to output the result.
-		/// </summary>
-		/// <param name="contents"></param>
-		/// <param name="context"></param>
-		/// <param name="controller"></param>
-		/// <param name="controllerContext"></param>
-		public override void RenderStaticWithinLayout(string contents, IEngineContext context, IController controller, IControllerContext controllerContext)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Should process the specified partial. The partial name must contains
-		/// the path relative to the views folder.
-		/// </summary>
-		/// <param name="partialName">The partial name.</param>
-		/// <param name="output">The output.</param>
-		/// <param name="context">The request context.</param>
-		/// <param name="controller">The controller.</param>
-		/// <param name="controllerContext">The controller context.</param>
-		public override void ProcessPartial(string partialName, TextWriter output, IEngineContext context, IController controller, IControllerContext controllerContext)
-		{
-			throw new NotImplementedException();
-		}
 
 		/// <summary>
 		/// Gets a value indicating whether the view engine
@@ -160,6 +39,127 @@ namespace Castle.MonoRail.Views.RubyView
 		public override string ViewFileExtension
 		{
 			get { return VIEW_FILE_EXTENSION; }
+		}
+
+		/// <summary>
+		/// Implementors should return a generator instance if
+		/// the view engine supports JS generation.
+		/// </summary>
+		/// <param name="generatorInfo">The generator info.</param>
+		/// <param name="context">The request context.</param>
+		/// <param name="controller">The controller.</param>
+		/// <param name="controllerContext">The controller context.</param>
+		/// <returns>A JS generator instance</returns>
+		public override object CreateJSGenerator(JSCodeGeneratorInfo generatorInfo, IEngineContext context,
+		                                         IController controller, IControllerContext controllerContext)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Processes the js generation view template - using the templateName
+		/// to obtain the correct template, and using the specified <see cref="T:System.IO.TextWriter"/>
+		/// to output the result.
+		/// </summary>
+		/// <param name="templateName">Name of the template.</param>
+		/// <param name="output">The output.</param>
+		/// <param name="generatorInfo">The generator info.</param>
+		/// <param name="context">The request context.</param>
+		/// <param name="controller">The controller.</param>
+		/// <param name="controllerContext">The controller context.</param>
+		public override void GenerateJS(string templateName, TextWriter output, JSCodeGeneratorInfo generatorInfo,
+		                                IEngineContext context, IController controller, IControllerContext controllerContext)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Processes the view - using the templateName
+		/// to obtain the correct template
+		/// and writes the results to the System.IO.TextWriter.
+		/// </summary>
+		/// <param name="templateName"></param>
+		/// <param name="output"></param>
+		/// <param name="context"></param>
+		/// <param name="controller"></param>
+		/// <param name="controllerContext"></param>
+		/// <exception cref="MonoRailException"><c>MonoRailException</c>.</exception>
+		public override void Process(string templateName, TextWriter output, IEngineContext context, IController controller,
+		                             IControllerContext controllerContext)
+		{
+			var viewFileName = string.Concat(templateName, ViewFileExtension);
+
+			var viewSource = ViewSourceLoader.GetViewSource(viewFileName);
+			if (viewSource == null)
+				throw new MonoRailException(404, "No such view", string.Format("Missing or invalid view: {0}", viewFileName));
+			var layoutNames = controllerContext.LayoutNames;
+
+			var scriptRuntime = IronRuby.CreateRuntime();
+			var scriptScope = scriptRuntime.CreateScope();
+			var scriptEngine = IronRuby.GetEngine(scriptRuntime);
+
+			var view = new RubyView(viewFileName, output, viewSource);
+			if (layoutNames != null)
+			{
+				var last = view;
+
+				for (int i = layoutNames.Length - 1; i >= 0; i--)
+				{
+					Debug.Write(i);
+					IViewSource layoutSource;
+					var layoutFileName = string.Concat("Layouts\\", layoutNames[i].Trim(), ViewFileExtension);
+					layoutSource = ViewSourceLoader.GetViewSource(layoutFileName);
+					if (layoutSource == null)
+						throw new MonoRailException(string.Format("Layout '{0}' cannot be found or loaded.", layoutNames[i].Trim()));
+					last.Parent = new RubyView(layoutFileName, output, layoutSource);
+					last = last.Parent;
+				}
+			}
+			view.Render();
+		}
+
+		/// <summary>
+		/// Processes the view - using the templateName
+		/// to obtain the correct template
+		/// and writes the results to the <see cref="T:System.IO.TextWriter"/>.
+		/// </summary>
+		/// <param name="templateName"></param>
+		/// <param name="layoutName"></param>
+		/// <param name="output"></param>
+		/// <param name="parameters"></param>
+		public override void Process(string templateName, string layoutName, TextWriter output,
+		                             IDictionary<string, object> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Wraps the specified content in the layout using the
+		/// context to output the result.
+		/// </summary>
+		/// <param name="contents"></param>
+		/// <param name="context"></param>
+		/// <param name="controller"></param>
+		/// <param name="controllerContext"></param>
+		public override void RenderStaticWithinLayout(string contents, IEngineContext context, IController controller,
+		                                              IControllerContext controllerContext)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Should process the specified partial. The partial name must contains
+		/// the path relative to the views folder.
+		/// </summary>
+		/// <param name="partialName">The partial name.</param>
+		/// <param name="output">The output.</param>
+		/// <param name="context">The request context.</param>
+		/// <param name="controller">The controller.</param>
+		/// <param name="controllerContext">The controller context.</param>
+		public override void ProcessPartial(string partialName, TextWriter output, IEngineContext context,
+		                                    IController controller, IControllerContext controllerContext)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
---
+++ trunk/src/test/Castle.MonoRail.Views.RubyView.PerformanceTests/NUnitTestFixture.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Castle.MonoRail.Views.RubyView.PerformanceTests
+{
+	public class NUnitTestFixture
+	{
+		[SetUp]
+		public virtual void SetUp()
+		{
+		}
+	}
+}
